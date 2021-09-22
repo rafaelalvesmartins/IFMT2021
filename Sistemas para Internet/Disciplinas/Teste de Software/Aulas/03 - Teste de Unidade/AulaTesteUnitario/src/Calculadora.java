26ee6ad3ec9ae079ddd459b5fe9eaaed0890568e
rafaelalvesmartins <rafaelmartinsalves@gmail.com>
2021-09-21 21:13:39 -0300

    Projeto Teste Unitário

---
+++ Sistemas para Internet/Disciplinas/Teste de Software/Aulas/03 - Teste de Unidade/AulaTesteUnitario/src/Calculadora.java
@@ -0,0 +1,20 @@
+
+public class Calculadora {
+	
+	double soma(double operando1, double operando2) {
+		return (operando1 + operando2);
+	}
+	
+	double subtracao(double operando1, double operando2) {
+		return (operando1 - operando2);
+	}
+	
+	double multiplicacao(double operando1, double operando2) {
+		return (operando1 * operando2);
+	}
+	
+	double divicao(double operando1, double operando2) {
+		return (operando1 / operando2);
+	}
+
+}
